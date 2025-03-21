f26038118a42ab86f1d7ca2bb21c347a61cbfd28
kienngynh <kien.ngynh@gmail.com>
2025-03-21 22:29:57 +0700

    Update

--- packages/i18n/src/constants/language.ts
+++ packages/i18n/src/constants/language.ts
@@ -4,19 +4,9 @@ export const FALLBACK_LANGUAGE: TLanguage = "en";
 
 export const SUPPORTED_LANGUAGES: ILanguageOption[] = [
   { label: "English", value: "en" },
-  { label: "Français", value: "fr" },
-  { label: "Español", value: "es" },
-  { label: "日本語", value: "ja" },
+  { label: "Tiếng Việt", value: "vn" },
   { label: "简体中文", value: "zh-CN" },
   { label: "繁體中文", value: "zh-TW" },
-  { label: "Русский", value: "ru" },
-  { label: "Italian", value: "it" },
-  { label: "Čeština", value: "cs" },
-  { label: "Slovenčina", value: "sk" },
-  { label: "Deutsch", value: "de" },
-  { label: "Українська", value: "ua" },
-  { label: "Polski", value: "pl" },
-  { label: "한국어", value: "ko" },
 ];
 
 export const STORAGE_KEY = "userLanguage";
--- packages/i18n/src/types/language.ts
+++ packages/i18n/src/types/language.ts
@@ -1,4 +1,4 @@
-export type TLanguage = "en" | "fr" | "es" | "ja" | "zh-CN" | "zh-TW" | "ru" | "it" | "cs" | "sk" | "de" | "ua" | "pl" | "ko";
+export type TLanguage = "en" | "vn" | "zh-CN" | "zh-TW";
 
 export interface ILanguageOption {
   label: string;
